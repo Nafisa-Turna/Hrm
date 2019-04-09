46538624711747a0d4398d90da736053cd445f9a
Nafisa-Turna <nafisaturna1999@gmail.com>
2019-04-09 17:01:23 +0600

    template dir correct

--- oursite/settings.py
+++ oursite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'oursite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
